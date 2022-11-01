4a45fe83dcae224ce19314d8e30f0b007ff1bc0c
nanqianglin <107928296+nanqianglin@users.noreply.github.com>
2022-11-01 12:45:59 +0000

    Deploying to gh-pages from @ HJFront/GotaBit-testnet@5f40fcc12e94c9584a88fa50f77a85ac1a5ff7d3 🚀

---
+++ _next/static/8bZ5iZ25nhlQpQXGC5_m1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/978-76d36497450cc6b4.js","static/chunks/pages/index-b3019048d34530ce.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/_Ybl4TPulVYZhoyxbi5Fh/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/890-17f2e06c974ae83d.js","static/chunks/pages/index-aee21bd20728872b.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
